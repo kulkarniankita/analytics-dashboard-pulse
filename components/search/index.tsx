32a26edce7f03b378f3e7c8e13e803a9cfa66cf2
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2025-05-13 12:44:35 -0400

    Lesson: Syncing Search Input to URL with onChange

--- components/search/index.tsx
+++ components/search/index.tsx
@@ -1,11 +1,25 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { commonStyles } from "@/styles/common";
 import Form from "next/form";
 import SearchStatus from "./search-status";
+import { useRouter, useSearchParams, useParams } from "next/navigation";
+import { InsightKey } from "@/@types/insights";
 
 export default function Search() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const params = useParams();
+  const activeTab = params.insight as InsightKey;
+
+  const defaultValue = searchParams.get("q") || "";
+
   return (
-    <Form action="" className="relative flex w-full flex-col gap-2 sm:w-fit">
+    <Form
+      action=""
+      className="relative flex w-full flex-col gap-2 sm:w-fit"
+      key={activeTab}
+    >
       <label
         className="font-bold uppercase tracking-wider text-black text-2xl"
         htmlFor="search"
@@ -17,6 +31,7 @@ export default function Search() {
           autoComplete="off"
           id="search"
           name="q"
+          defaultValue={defaultValue}
           className={cn(
             "w-full pl-10 pr-4 py-3 sm:w-96",
             "focus:outline-none focus:shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]",
@@ -25,6 +40,15 @@ export default function Search() {
           )}
           placeholder="Search in task title or description..."
           type="search"
+          onChange={(e) => {
+            const newSearchParams = new URLSearchParams(
+              searchParams.toString()
+            );
+            newSearchParams.set("q", e.target.value);
+            router.push(`?${newSearchParams.toString()}`, {
+              scroll: false,
+            });
+          }}
         />
         <SearchStatus />
       </div>
